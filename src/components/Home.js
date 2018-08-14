c0fbda13f86e5aece4014d12ad8b7e2758eda3e5
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-14 11:16:02 -0600

    added router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import {link} from 'react-router-dom'
-import{Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {
+  Jumbotron,
+  Grid,
+  Row,
+  Col,
+  Image,
+  Button
+} from 'react-bootstrap'
 import './Home.css'
 
-
-export default class Home extends Component{
-  render(){
-    return(
+export default class Home extends Component {
+  render() {
+    return (
       <Grid>
-        <Jumbotron>
-          <h2>Test</h2>
-          <p>this works</p>
-        </Jumbotron>
-      </Grid>
+      <Jumbotron>
+        <h2>This WORKS</h2>
+        <p>Test</p>
+        <Link to="/about">
+          <Button bsStyle="primary">Learn More</Button>
+        </Link>
+      </Jumbotron>
+    </Grid>
     )
   }
 }
