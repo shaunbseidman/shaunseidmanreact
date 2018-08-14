7c3c96361e08c04d062459f92a978e04f0e2d63b
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-14 12:04:32 -0600

    added profile picture

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import './CustomNavbar.css'
 
 export default class CustomNavbar extends Component {
   render() {
@@ -8,17 +9,17 @@ export default class CustomNavbar extends Component {
       <Navbar default collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to='/'>Back Home</Link>
+              <Link to='/'>Home</Link>
             </Navbar.Brand>
             <Navbar.Toggle/>
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav pullRight="pullRight">
+            <Nav pullRight>
               <NavItem eventKey={1} componentClass={Link} href='/' to='/'>
                 Home
               </NavItem>
               <NavItem eventKey={2} componentClass={Link} href='/' to='/about'>
-                about
+                About
               </NavItem>
               <NavItem eventKey={3} componentClass={Link} href='/' to='/news'>
                 News
--- src/components/Home.js
+++ src/components/Home.js
@@ -21,6 +21,20 @@ export default class Home extends Component {
           <Button bsStyle="primary">Learn More</Button>
         </Link>
       </Jumbotron>
+      <Row className='show-grid text-center'>
+        <Col xs={12} sm={4} className='person-wrapper'>
+          <Image src='assets/person-1.jpg' circle className='profile-pic' />
+          <p>fart</p>
+        </Col>
+        <Col xs={12} sm={4} className='person-wrapper'>
+          <Image src='assets/person-1.jpg' circle className='profile-pic' />
+          <p>fart</p>
+        </Col>
+        <Col xs={12} sm={4} className='person-wrapper'>
+          <Image src='assets/person-1.jpg' circle className='profile-pic' />
+          <p>fart</p>
+        </Col>
+      </Row>
     </Grid>
     )
   }
