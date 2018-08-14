d636fc07f80880a5b06c922a2dbb696f29abe99e
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-14 12:46:29 -0600

    fimished inital styling

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default class About extends Component {
         <Grid>
           <Col xs={12} sm={8} smOffset={2}>
             <Image src="assets/shaun2.jpg" className="about-profile-pic" rounded />
-            <h3>Frank The Tank</h3>
+            <h3>Poop</h3>
             <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out. These trees are so much fun. I get started on them and I have a hard time stopping. How to paint. That's easy. What to paint. That's much harder. Be brave. The man who does the best job is the one who is happy at his job.</p>
             <p>Anyone can paint. You can do anything here. So don't worry about it. Life is too short to be alone, too precious. Share it with a friend. Every highlight needs it's own personal shadow.</p>
             <p>That's what makes life fun. That you can make these decisions. That you can create the world that you want. Of course he's a happy little stone, cause we don't have any other kind. It's so important to do something every day that will make you happy.</p>
